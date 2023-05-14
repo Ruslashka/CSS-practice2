12d150687e78450bfb0fbf160e4717c694251e7e
Ruslashka <chotakoe1996@gmail.com>
2023-05-14 14:41:56 +0300

    upd packages

--- src/route/index.js
+++ src/route/index.js
@@ -588,11 +588,11 @@ router.get('/car', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/bootstrap', function (req, res) {
+router.get('/CssSelector1', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('bootstrap', {
+  res.render('CssSelector1', {
     layout: 'basic',
   })
   //                  ↑↑ сюди вводимо JSON дані
